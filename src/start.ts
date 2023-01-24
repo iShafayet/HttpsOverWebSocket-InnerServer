457869b9fbccfc3d7082b524c26fb0f86a57e67f
Sayem Shafayet <ishafayet@gmail.com>
2023-01-24 22:02:54 +0600

    Revamp logger

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,26 @@
 import { HttpsOverWebSockerOuterServer } from "./index.js";
 import { Config } from "./lib/config.js";
+import { Logger } from "./lib/logger.js";
 import {
   extractProcessParams,
   lookupAndLoadConfigAsync,
 } from "./utility/startup-utils.js";
 
+// We initiate logger and inject it into global so that it is usable everywhere.
+global.logger = new Logger({
+  switches: {
+    debug: true,
+    log: true,
+    important: true,
+    warning: true,
+    error: true,
+    critical: true,
+  },
+});
+logger.init();
+
 let commandLineParams = extractProcessParams();
-console.log("STARTUP Application parameters: ", commandLineParams);
+logger.log("STARTUP Application parameters: ", commandLineParams);
 
 let config: Config = lookupAndLoadConfigAsync(commandLineParams);
 new HttpsOverWebSockerOuterServer().start(config);
